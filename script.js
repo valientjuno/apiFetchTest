42be5edafabdc86956900ec71a45febe70ef6157
valientjuno <jessedoake91@gmail.com>
2025-08-06 10:46:56 -0600

    images descriptions and addresses

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ fetch(URL)
   .then((response) => response.json())
   .then((jsObject) => {
     let business = jsObject.business;
-
+    // const business = jsObject.business;
+    console.log(business);
     for (let i = 0; i < business.length; i++) {
       // console.log(business[i].name);
       // You must include your javascript below to display the following information:
@@ -23,12 +24,25 @@ fetch(URL)
       let h2 = document.createElement("h2");
       h2.textContent = business[i].name;
       card.appendChild(h2);
-      document.querySelector("div.cards").appendChild(card);
+      // document.querySelector("div.cards").appendChild(card);
 
       // Create the image location, you can look up how to add image using javascript
-
+      let myImage = document.createElement("img");
+      myImage.src = business[i].imageurl;
+      myImage.alt = "Buisness Images";
+      myImage.height = "200";
+      card.appendChild(myImage);
       // Include a business location
 
+      let location = document.createElement("p");
+      location.textContent = business[i].address;
+      card.appendChild(location);
+
       // Include a business Description
+
+      document.querySelector("div.cards").appendChild(card);
+      let descriptions = document.createElement("p");
+      descriptions.textContent = business[i].description;
+      card.appendChild(descriptions);
     }
   });
